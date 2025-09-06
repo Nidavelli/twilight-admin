6dd26d7e12f5decec27453615f62cbf7e437b6db
Nida <tmarn2004@gmail.com>
2025-09-06 12:58:59 +0300

    2trial

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
 
 export default defineConfig({
-  base: "./",
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "pages/*",
-          dest: "pages",
-        },
-        {
-          src: "favicon.ico",
-          dest: ".",
-        },
-      ],
-    }),
-  ],
+  root: "./",
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        listItem: path.resolve(__dirname, "pages/list-items.html"),
+        orders: path.resolve(__dirname, "pages/orders.html"),
+      },
+    },
+  },
+
+  resolve: {
+    alias: {
+      "html5-qrcode": "html5-qrcode",
+    },
+  },
 });
